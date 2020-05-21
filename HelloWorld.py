f9c48444c21fb3248011adf7f05aa7b1c74725c2
Eric Lambert <elambertg@gmail.com>
2020-05-20 23:27:55 -0400

    Fix paths

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,7 +5,7 @@ def main():
 
     tracks = []
 
-    for root, dir, files, in os.walk("D:\My\Music\Mix 2020"):
+    for root, dir, files, in os.walk("D:\\My\\Music\\Mix 2020"):
         for name in files:
             if name.endswith(".mp3"):
                 print(name)
